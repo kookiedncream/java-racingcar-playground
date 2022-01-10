1ef0ff1adf006f24a5f4864370d314fe3bb548b6
kookiedncream <kim76744@naver.com>
2022-01-10 14:19:20 +0900

    feat: ,나 ;로 구별한 값을 더해서 반환

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -11,7 +11,23 @@ public class StringAddCalculator {
 			return Integer.parseInt(text);
 		}
 
-		return 0;
+		String[] split = text.split(",|:");
+		int sum = 0;
+
+		if (split.length > 1) {
+			sum = getSum(sum, split);
+			return sum;
+		}
+
+		return sum;
+	}
+
+	private static int getSum(int sum, String[] tokens) {
+		int length = tokens.length;
+		for (int i = 0; i < length; i++) {
+			sum += Integer.parseInt(tokens[i]);
+		}
+		return sum;
 	}
 
 	private static boolean isItOneWord(String text) {
